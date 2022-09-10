78605638fdf3e83ea179e203638a1ce288aba284
arweya <anitarueya@gmail.com>
2022-09-10 08:27:04 -0700

    4 put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
+ * Return:Always 0 (Sucess)
+ */
 
 int main(void)
 
-{
-
+{      
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 
 }
