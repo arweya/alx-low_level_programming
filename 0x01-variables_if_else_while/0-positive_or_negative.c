93b4fa28597ddb87a3fc348ad3c35099d5ea50e4
arweya <anitarueya@gmail.com>
2022-09-13 02:31:48 -0700

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,17 +12,13 @@
  *    Description: print value of n satus; zero, positive ornegative
  *
  *     Return: Always 0 (Success)
- *
- *  
  */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
-	/*my code*/
-
+/*my code*/
 if (n > 0)
 
 printf("%i is positive\n", n);
