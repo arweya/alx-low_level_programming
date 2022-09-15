9b365844efda6354bc4890dc09c4a69c17019225
arweya <anitarueya@gmail.com>
2022-09-15 04:16:09 -0700

    0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,3 @@
-#ifndef HOLBERTON_INCLUDED
-#define HOLBERTON _H_ INCLUDED
-
-int.putchar(char);
-voidprint_alphabet(void)
-voidprint_alphabet_x10(void)
-int_islower(int c);
-int_is alpha(int c);
-int print_sign(int n);
-int_abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-void num (int r, int n);
-#end if
-
 #include "main.h"
 
 /**
