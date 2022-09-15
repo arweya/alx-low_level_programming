790b34ee26c7c3588cb6232b98b4fdcd797ff1a2
arweya <anitarueya@gmail.com>
2022-09-15 04:06:35 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,36 +1,43 @@
+#ifndef HOLBERTON_INCLUDED
+#define HOLBERTON _H_ INCLUDED
+
+int.putchar(char);
+voidprint_alphabet(void)
+voidprint_alphabet_x10(void)
+int_islower(int c);
+int_is alpha(int c);
+int print_sign(int n);
+int_abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num (int r, int n);
+#end if
+
 #include "main.h"
 
 /**
- *main -prints Putchar
+ * main- print Putchar
  *
- *Description:prints_putchar
- *    
- *Return: 0 (Success)
+ * Description: prints_putchar
+ *
+ * Return: 0 (Sucess)
  */
-int main(void)
+
+int main (void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-		_putchar('_');
-                
-		_putchar('p');
-                      
-		_putchar('u');
-                                
-		_putchar('t');
-                                       
-		_putchar('c');
-                                                
-	        _putchar('h');
-                                                       
-                _putchar('a');
-                                                              
-	        _putchar('r');
-
-		_putchar('\n');
-
-return (0);
+	return (0);
 }
-
-
-
-
