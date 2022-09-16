2f622fde3a77e926c7baaa90ba95fefa092cc510
arweya <anitarueya@gmail.com>
2022-09-16 07:32:25 -0700

    3 print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function declared
+ * Return: 0
+ */
+void print_numbers(void)
+{
+		int c;
+
+		for (c = 48; c < 58; c++)
+		{
+                _putchar(c);
+		}                                                 
+		_putchar('\n');
+}
