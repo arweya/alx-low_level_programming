0b891028710b65124af88c150c0631f61c8f0944
arweya <anitarueya@gmail.com>
2022-09-16 04:37:12 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,16 +3,16 @@
 
 int _putchar (char);
 void print alphabet (void );
-void print_alphabet_x1 0(void);
+void print_alphabet_x10(void);
 int _islower (int c);
 int _isalpha(int c);
 int print_sign (int n);
 int _abs(int);
-int print last_digit(int );
-void jack_bauer (void );
-void times_table(void );
+int print last_digit(int);
+void jack_bauer (void);
+void times_table(void);
 int add (int a, int b);
-void pr int_to_98(int n);
+void print_to_98(int n);
 void pr1nt_times_table(int n);
 
 #endif
