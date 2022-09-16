e7815d5ef405896c5171ae1153a512c7528a53dd
arweya <anitarueya@gmail.com>
2022-09-16 03:50:17 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar (char);
+void print alphabet (void );
+void print_alphabet_x1 0(void);
+int _islower (int c);
+int _isalpha(int c);
+int print_sign (int n);
+int _abs(int);
+int print last_digit(int );
+void jack_bauer (void );
+void times_table(void );
+int add (int a, int b);
+void pr int_to_98(int n);
+void pr1nt_times_table(int n);
+
+#endif
