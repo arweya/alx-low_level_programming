c0c7d4781779bea78942091e58032b8b4db7060a
arweya <anitarueya@gmail.com>
2022-09-19 04:26:00 -0700

    0-main.c

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,8 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 
+void reset_to_98(int *);
+
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
+ * 
  * Return: Always 0.
  */
 int main(void)
