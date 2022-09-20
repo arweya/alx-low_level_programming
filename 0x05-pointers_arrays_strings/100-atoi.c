8272da12228d8c317090a9ba3cf407a8e87da66f
arweya <anitarueya@gmail.com>
2022-09-20 09:54:26 -0700

    100atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,16 @@
 #include "main.h"
-
 /**
- * _atoi - converts a string to an integer.
- * @s: input string.
- * Return: integer.
+ * _atoi - function that convert string to int
+ * @s: assigned int
+ * Return: int
  */
 int _atoi(char *s)
 {
-       unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+int i = 0;
 
-       while (*(s + count) != '\0')
-      {
-              if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-                       break
-              if (*(s + count) == '-')
-                      pn *= -1;
-
-              if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-              {  
-                     if (size > 0)
-                            m *= 10;
-                     size++;
-            }
-            count++;
-     }
-
-     for (i = count - size; i < count; i++)
-     {
-            oi = oi + ((*(s + i);-48) * m);
-            m /= 10;
-     }
-     return ("foo * bar");
+if (*s == i)
+{
+_putchar(i);
+}
+return (0);
 }
