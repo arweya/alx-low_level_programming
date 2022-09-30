d611f551047bf9a0981717e53c5df9b03599b1b8
arweya <anitarueya@gmail.com>
2022-09-30 04:44:15 -0700

    1alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,15 @@
  *
  * Return:void
  */
+
 voidprint_alphabet(void)
 {
-	int i;
+int i;
 
-	for (i= 'a'; i <= 'z'; i++)
-       	
-         {
-	       _putchar(i);
-         }
-	 _putchar('\n');
+for (i = 'a'; i <= 'z'; i++)
+
+{
+_putchar(i);
+}
+_putchar('\n');
 }
