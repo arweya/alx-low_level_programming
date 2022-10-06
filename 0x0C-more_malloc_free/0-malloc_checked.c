c4a86e1699f172a12cfabb5f2c8625b5d5ed064e
arweya <anitarueya@gmail.com>
2022-10-06 06:30:00 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,24 @@
+#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * malloc_checked - array for prints a string
- * @b:number of memory
- * Return: void
+ * malloc_checked - Function that allocates memory using malloc.
+ * @b: size to allocate memory.
+ * main - check the code
+ *
+ * Return: a pointer with the memory location.
  */
-void *malloc_checked(unsigned int b);
-{ 
-void *p = malloc(b);
-if (p == NULL)
+void *malloc_checked(unsigned int b)
+{
+void *ptr = NULL;
+
+ptr = malloc(b);
+
+if (ptr == NULL)
+{
 exit(98);
-return (p);
+}
+
+return (ptr);
 }
