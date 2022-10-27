b35d6fec67dd4a690cf6b15d52a58e8485073d57
arweya <anitarueya@gmail.com>
2022-10-27 00:52:30 -0700

    9insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,41 +1,50 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - ...
- * @head: ...
- * @idx: ...
- * @n: ...
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
  *
- * Return: ...
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new_node, *iter;
-unsigned int count = 1;
+unsigned int i;
+listint_t *new;
+listint_t *h;
 
-if (head)
+h = *head;
+
+if (idx != 0)
 {
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
+for (i = 0; i < idx - 1 && h != NULL; i++)
+{
+h = h->next;
+}
+}
+
+if (h == NULL && idx != 0)
 return (NULL);
 
-new_node->n = n;
-if (idx > 0)
-{
-iter = *head;
-while (iter)
-{
-if (count == idx)
-{
-new_node->next = iter->next;
-iter->next = new_node;
-return (new_node);
-}
-iter = iter->next;
-count++;
-}
-if (idx > count)
+new = malloc(sizeof(listint_t));
+if (new == NULL)
 return (NULL);
+new->n = n;
+
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+}
+else
+{
+new->next = h->next;
+h->next = new;
+}
+
+return (new);
 }
