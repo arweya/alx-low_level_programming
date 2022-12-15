f7167ff2e8d99cc4e07597feb2055ef1f5134097
arweya <anitarueya@gmail.com>
2022-12-15 04:39:03 -0800

    update 0-print-dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,14 @@
 size_t print_dlistint(const dlistint_t *h)
 {
         int count; 
+ 
+        count = 0;
 
-	if (h == NULL) 
+	if (h == NULL)	
 	        return (count);
 
 	while (h->prev != NULL)	      
-		h = h->next;
+		h = h->prev;
 
 	while (h != NULL)
 	{		
